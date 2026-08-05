62076998c7b87ac97ce249bc3895b44bf9972d1f
spott226 <uses88767@gmail.com>
2026-08-04 20:20:49 -0600

    Fix Meta webhook auth wiring

--- apps/api/src/meta-webhook/meta-webhook.module.ts
+++ apps/api/src/meta-webhook/meta-webhook.module.ts
@@ -1,5 +1,6 @@
 import { Module } from "@nestjs/common";
 import { AIProviderService } from "../ai-provider.service";
+import { AuthModule } from "../auth.module";
 import { PrismaService } from "../prisma.service";
 import { ResourceService } from "../resource.service";
 import { MetaOAuthController } from "./meta-oauth.controller";
@@ -8,6 +9,7 @@ import { MetaWebhookController } from "./meta-webhook.controller";
 import { MetaWebhookService } from "./meta-webhook.service";
 
 @Module({
+  imports: [AuthModule],
   controllers: [MetaWebhookController, MetaOAuthController],
   providers: [MetaWebhookService, MetaOAuthService, PrismaService, ResourceService, AIProviderService],
   exports: [MetaWebhookService, MetaOAuthService],
